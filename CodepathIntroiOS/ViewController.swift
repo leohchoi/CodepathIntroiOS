2342440f5c54962aee89507fe939532952bf6bf0
leohchoi <choi.leo2002@gmail.com>
2023-01-04 22:49:30 -0500

    finished code and customized

--- CodepathIntroiOS/ViewController.swift
+++ CodepathIntroiOS/ViewController.swift
@@ -7,6 +7,23 @@
 
 import UIKit
 
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+
+extension UIColor {
+    static func random() -> UIColor {
+        return UIColor(
+           red:   .random(),
+           green: .random(),
+           blue:  .random(),
+           alpha: 1.0
+        )
+    }
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var morePetsSwitch: UISwitch!
@@ -17,10 +34,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var yearSegmentedControl: UISegmentedControl!
     @IBOutlet weak var numberOfPetsLabel: UILabel!
     
+    @IBOutlet weak var hobbiesTextField: UITextField!
+    
+    @IBAction func changeBackgroundColorTapped(_ sender: UIButton) {
+        self.view.backgroundColor = .random()
+    }
+    
     @IBAction func introduceSelfDidTap(_ sender: UIButton) {
         let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)?.lowercased()
-        let introduction = "Hey guys! My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) pet(s). It is \(morePetsSwitch.isOn) that I want more pets. My hobbies include cafe hopping and just chilling with my friends, while also coding and learning new technologies here and there :D"
-//        print(introduction)
+        
+        let introduction = "Hey guys! My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) pet(s). It is \(morePetsSwitch.isOn) that I want more pets. My hobbies include \(hobbiesTextField.text!) :D"
+
         let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Nice to meet you!", style: .default, handler: nil)
@@ -33,6 +57,7 @@ class ViewController: UIViewController {
     @IBAction func stepperDidChange(_ sender: UIStepper) {
         numberOfPetsLabel.text = "\(Int(sender.value))"
     }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
--- README.md
+++ README.md
@@ -10,8 +10,6 @@ This is the prework for CodePath IOS102. This self-introduction app allows us to
 
 <!-- <img src="YOUR_GIF_URL_HERE" width=200><br> OR <img src="YOUR_GIF_PATH" width=200><br> -->
 
-`TODO://` In the Features section below, add an `x` between the `-[ ]` like this `- [x]` for any features you complete. (☝️ Remove this paragraph after after checking off user stories)
-
 ### Required Features
 
 - [x] 1. App displays an image of a school's logo
@@ -23,6 +21,6 @@ This is the prework for CodePath IOS102. This self-introduction app allows us to
 
 ### Optional Features
 
-- [ ] 1. User can tap a button to change the color of the background view
-- [ ] 3. User can select on additional buttons that provide more info about the user. Example: more textfields, a different alert box, etc.
-- [ ] 4. Any stylistic changes that are not default options (Comment this here)
+- [x] 1. User can tap a button to change the color of the background view
+- [x] 3. User can select on additional buttons that provide more info about the user. Example: more textfields, a different alert box, etc.
+- [x] 4. Changed default background color to mint and accent color to black. Then I filled the buttons to have a gray filling
